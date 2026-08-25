ebd0ab8a049807829d85ec5e6b785e38177fee98
Marcos Melo <marcosvnmelo.dev@gmail.com>
2026-08-24 20:21:15 -0400

    fix: update image configuration to allow local IP access based on base URL

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@ import type { NextConfig } from 'next';
 
 import { env } from '@/env';
 
-const isDev = env.NODE_ENV === 'development';
-
 const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     remotePatterns: [new URL(`${env.NEXT_PUBLIC_BASE_URL}/**`)],
-    dangerouslyAllowLocalIP: isDev,
+    dangerouslyAllowLocalIP: env.NEXT_PUBLIC_BASE_URL.includes('localhost'),
   },
 };
 
